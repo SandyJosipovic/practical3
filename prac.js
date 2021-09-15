04b306a778f4eaed4b9c63ad2a817369cf037b7e
SandyJosipovic <87452807+SandyJosipovic@users.noreply.github.com>
2021-09-16 01:16:34 +1000

    Add files via upload

---
+++ prac.js
@@ -0,0 +1,15 @@
+var prac1 = new Vue({
+  el: '#prac-1',
+  data: {
+    todos: [{
+        text: 'Finish all practicls'
+      },
+      {
+        text: 'Submit to GitHub'
+      },
+      {
+        text: 'Submit to Deakin'
+      }
+    ]
+  }
+})
